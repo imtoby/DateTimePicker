8bd889108ca804ea0e9ad8aac89e8b4132024c87
imtoby <imtoby@126.com>
2017-12-04 20:01:20 +0800

    add function

--- DateTimePicker/main.cpp
+++ DateTimePicker/main.cpp
@@ -1,5 +1,23 @@
 #include <QGuiApplication>
 #include <QQmlApplicationEngine>
+#include <QDate>
+#include <QStringList>
+#include <QDebug>
+
+QDate StartDate(1970, 1, 1);
+QDate EndDate(1950, 12, 31);
+QStringList DateList;
+
+void InitDateList() {
+    QDate date = StartDate;
+    DateList.append(date.toString("yyyy-MM-ddd"));
+    while (date < EndDate) {
+        date.addDays(1);
+        DateList.append(date.toString("yyyy-MM-ddd"));
+    }
+
+    qDebug() << DateList;
+}
 
 int main(int argc, char *argv[])
 {
@@ -7,6 +25,7 @@ int main(int argc, char *argv[])
 
     QQmlApplicationEngine engine;
     engine.load(QUrl(QStringLiteral("qrc:/main.qml")));
+    InitDateList();
 
     return app.exec();
 }
--- DateTimePicker/src/DataModelManager.cpp
+++ DateTimePicker/src/DataModelManager.cpp
@@ -1,4 +1,23 @@
 #include "DataModelManager.h"
+#include <QDate>
+#include <QStringList>
+
+namespace {
+
+QDate StartDate(1970, 1, 1);
+QDate EndDate(1950, 12, 31);
+QStringList DateList;
+
+void InitDateList() {
+    QDate date = StartDate;
+    DateList.append(date.toString("yyyy-MM-ddd"));
+    while (date < EndDate) {
+        date.addDays(1);
+        DateList.append(date.toString("yyyy-MM-ddd"));
+    }
+}
+
+}
 
 class DataModelManagerPrivate
 {
